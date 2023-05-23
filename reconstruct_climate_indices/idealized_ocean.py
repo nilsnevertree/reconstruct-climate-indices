c508ded04c4b6f6d46865ac4d32e1dd95fc4a2a8
nilsnevertree <nils.niebaum@gmx.de>
2023-05-23 09:50:25 +0200

    realistic idealized model added

--- reconstruct_climate_indices/idealized_ocean.py
+++ reconstruct_climate_indices/idealized_ocean.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 # idealized.ipynb
 # DESCRIPTION
 # -----------
@@ -34,15 +34,12 @@ import numpy as np
 import xarray as xr
 
 
-#%%
+# %%
 
-def spunge_ocean(nt=1000,
-    dt=365.25,
-    df=1.15e-1,
-    tau0=10 * 365.25,
-    save_path=None,
-    seed=331381460666):
 
+def spunge_ocean(
+    nt=1000, dt=365.25, df=1.15e-1, tau0=10 * 365.25, save_path=None, seed=331381460666
+):
     dW = np.sqrt(dt)  # (sqrt (days)) Stochastic time step
 
     # Precomputation
@@ -60,22 +57,26 @@ def spunge_ocean(nt=1000,
         fi = random_forcing[it]
         SAT[it] = fi
         time[it] = time[it - 1] + dt
-        SST[it] = (
-            SST[it - 1] + (-l0 * SST[it - 1]) * dt + (SAT[it - 1]) * dW
-        )
+        SST[it] = SST[it - 1] + (-l0 * SST[it - 1]) * dt + (SAT[it - 1]) * dW
     timep = time / 365.25  # (yr) TIME for plot
 
     ds = xr.Dataset(
-    coords=dict(
-        time=(["time"], time),
-        time_years=(["time"], timep),
-        ocean_restoring_timescale= (["ocean_restoring_timescale"], [tau0]),
-        stochastic_forcing_intensity= (["stochastic_forcing_intensity"], [df]),
+        coords=dict(
+            time=(["time"], time),
+            time_years=(["time"], timep),
+            ocean_restoring_timescale=(["ocean_restoring_timescale"], [tau0]),
+            stochastic_forcing_intensity=(["stochastic_forcing_intensity"], [df]),
         ),
         data_vars=dict(
             random_forcing=(["time"], random_forcing),
-            surface_air_temperature=(["time", "ocean_restoring_timescale", "stochastic_forcing_intensity"], SAT[:, np.newaxis, np.newaxis]),
-            sponge_sea_surface_temperature=(["time", "ocean_restoring_timescale", "stochastic_forcing_intensity"], SST[:, np.newaxis, np.newaxis]),
+            surface_air_temperature=(
+                ["time", "ocean_restoring_timescale", "stochastic_forcing_intensity"],
+                SAT[:, np.newaxis, np.newaxis],
+            ),
+            sponge_sea_surface_temperature=(
+                ["time", "ocean_restoring_timescale", "stochastic_forcing_intensity"],
+                SST[:, np.newaxis, np.newaxis],
+            ),
         ),
         attrs=dict(
             coder="Florian Sévellec <florian.sevellec@univ-brest.fr>",
@@ -88,14 +89,15 @@ def spunge_ocean(nt=1000,
         return ds
 
 
-def oscillatory_ocean(nt=1000,
+def oscillatory_ocean(
+    nt=1000,
     dt=365.25,
     df=1.15e-1,
     per0=24 * 365.25,
     tau0=10 * 365.25,
     save_path=None,
-    seed=331381460666):
-
+    seed=331381460666,
+):
     dW = np.sqrt(dt)  # (sqrt (days)) Stochastic time step
 
     # Precomputation
@@ -123,19 +125,42 @@ def oscillatory_ocean(nt=1000,
     timep = time / 365.25  # (yr) TIME for plot
 
     ds = xr.Dataset(
-    coords=dict(
-        time=(["time"], time),
-        time_years=(["time"], timep),
-        ocean_restoring_timescale= (["ocean_restoring_timescale"], [tau0]),
-        ocean_oscillation_timescale= (["ocean_oscillation_timescale"], [per0]),
-        stochastic_forcing_intensity= (["stochastic_forcing_intensity"], [df]),
-
+        coords=dict(
+            time=(["time"], time),
+            time_years=(["time"], timep),
+            ocean_restoring_timescale=(["ocean_restoring_timescale"], [tau0]),
+            ocean_oscillation_timescale=(["ocean_oscillation_timescale"], [per0]),
+            stochastic_forcing_intensity=(["stochastic_forcing_intensity"], [df]),
         ),
         data_vars=dict(
             random_forcing=(["time"], random_forcing),
-            surface_air_temperature=(["time", "ocean_restoring_timescale", "ocean_oscillation_timescale", "stochastic_forcing_intensity"], SAT[:, np.newaxis, np.newaxis, np.newaxis]),
-            oscillator_sea_surface_temperature=(["time", "ocean_restoring_timescale", "ocean_oscillation_timescale", "stochastic_forcing_intensity"], SST[:, np.newaxis, np.newaxis, np.newaxis]),
-            oscillator_deep_ocean_temperature=(["time", "ocean_restoring_timescale", "ocean_oscillation_timescale", "stochastic_forcing_intensity"], DOT[:, np.newaxis, np.newaxis, np.newaxis]),
+            surface_air_temperature=(
+                [
+                    "time",
+                    "ocean_restoring_timescale",
+                    "ocean_oscillation_timescale",
+                    "stochastic_forcing_intensity",
+                ],
+                SAT[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
+            oscillator_sea_surface_temperature=(
+                [
+                    "time",
+                    "ocean_restoring_timescale",
+                    "ocean_oscillation_timescale",
+                    "stochastic_forcing_intensity",
+                ],
+                SST[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
+            oscillator_deep_ocean_temperature=(
+                [
+                    "time",
+                    "ocean_restoring_timescale",
+                    "ocean_oscillation_timescale",
+                    "stochastic_forcing_intensity",
+                ],
+                DOT[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
         ),
         attrs=dict(
             coder="Florian Sévellec <florian.sevellec@univ-brest.fr>",
@@ -148,23 +173,27 @@ def oscillatory_ocean(nt=1000,
         return ds
 
 
-def realistic_AMO_oscillatory_ocean(nt=1000,
+def AMO_oscillatory_ocean(
+    nt=1000,
     dt=365.25,
     per0=24 * 365.25,
     tau0=10 * 365.25,
-    dNAO = 0.1, # (K days-1/2) stochastic amplitude of NAO
-    dEAP = 0.1, # (K days-1/2) stochastic amplitude of EAP
-    cNAOvsEAP = 0, # (K^2 days) Covariance of NAO and EAP
+    dNAO=0.1,  # (K days-1/2) stochastic amplitude of NAO
+    dEAP=0.1,  # (K days-1/2) stochastic amplitude of EAP
+    cNAOvsEAP=0,  # (K^2 days) Covariance of NAO and EAP
     save_path=None,
-    seed=331381460666):
-
+    seed=331381460666,
+):
     dW = np.sqrt(dt)  # (sqrt (days)) Stochastic time step
 
     # Precomputation
     l0 = 2 / tau0  # (days-1) inverse restoring timescale
     o0 = 2 * 3.14 / per0  # (days-1) inverse oscillation timescale
-    A=[[dNAO**2,cNAOvsEAP],[cNAOvsEAP,dEAP**2]]# Covariance Matrix of ztmospheric forcing
-    L=np.linalg.cholesky(A) # Cholesky factorization of the Covariance Matrix
+    A = [
+        [dNAO**2, cNAOvsEAP],
+        [cNAOvsEAP, dEAP**2],
+    ]  # Covariance Matrix of ztmospheric forcing
+    L = np.linalg.cholesky(A)  # Cholesky factorization of the Covariance Matrix
 
     # Initialization
     NAO = np.zeros(nt)
@@ -179,32 +208,47 @@ def realistic_AMO_oscillatory_ocean(nt=1000,
     for it in np.arange(1, nt):
         time[it] = time[it - 1] + dt
         # AMO-type Ocean oscillation
-        ft=rng.standard_normal(2)
-        ftt=np.matmul(ft, A)
+        ft = rng.standard_normal(2)
+        ftt = np.matmul(ft, A)
         NAO[it] = ftt[0]
         EAP[it] = ftt[1]
         AMO[it] = (
-            AMO[it - 1] + (-l0 * AMO[it - 1] - o0 * ZOT[it - 1]) * dt + (EAP[it - 1]) * dW
+            AMO[it - 1]
+            + (-l0 * AMO[it - 1] - o0 * ZOT[it - 1]) * dt
+            + (EAP[it - 1]) * dW
         )
         ZOT[it] = (
-            ZOT[it - 1] + (-l0 * ZOT[it - 1] + o0 * AMO[it - 1]) * dt + (NAO[it - 1]) * dW
+            ZOT[it - 1]
+            + (-l0 * ZOT[it - 1] + o0 * AMO[it - 1]) * dt
+            + (NAO[it - 1]) * dW
         )
         timep = time / 365.25  # (yr) TIME for plot
 
-
     ds = xr.Dataset(
-    coords=dict(
-        time=(["time"], time),
-        time_years=(["time"], timep),
-        dEAP= (["dEAP"], [dEAP]),
-        dNAO= (["dNAO"], [dNAO]),
-        cNAOvsEAP= (["cNAOvsEAP"], [cNAOvsEAP]),
+        coords=dict(
+            time=(["time"], time),
+            time_years=(["time"], timep),
+            dEAP=(["dEAP"], [dEAP]),
+            dNAO=(["dNAO"], [dNAO]),
+            cNAOvsEAP=(["cNAOvsEAP"], [cNAOvsEAP]),
         ),
         data_vars=dict(
-            NAO=(["time", "dEAP", "dEAP", "cNAOvsEAP"], NAO[:, np.newaxis, np.newaxis, np.newaxis]),
-            EAP=(["time", "dEAP", "dEAP", "cNAOvsEAP"], EAP[:, np.newaxis, np.newaxis, np.newaxis]),
-            ZOT=(["time", "dEAP", "dEAP", "cNAOvsEAP"], ZOT[:, np.newaxis, np.newaxis, np.newaxis]),
-            AMO=(["time", "dEAP", "dEAP", "cNAOvsEAP"], AMO[:, np.newaxis, np.newaxis, np.newaxis]),
+            NAO=(
+                ["time", "dEAP", "dEAP", "cNAOvsEAP"],
+                NAO[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
+            EAP=(
+                ["time", "dEAP", "dEAP", "cNAOvsEAP"],
+                EAP[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
+            ZOT=(
+                ["time", "dEAP", "dEAP", "cNAOvsEAP"],
+                ZOT[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
+            AMO=(
+                ["time", "dEAP", "dEAP", "cNAOvsEAP"],
+                AMO[:, np.newaxis, np.newaxis, np.newaxis],
+            ),
         ),
         attrs=dict(
             coder="Florian Sévellec <florian.sevellec@univ-brest.fr>",
@@ -226,21 +270,23 @@ def integrate_idealized_ocean(
     save_path=None,
     seed=331381460666,
 ):
-    spunge = spunge_ocean(nt=time_steps,
-                          dt=dt,
-                          df=stochastic_forcing_intensity,
-                          tau0=ocean_restoring_timescale,
-                          seed=seed,
-                          save_path=None
-                          )
-    oscillator = oscillatory_ocean(nt=time_steps,
-                          dt=dt,
-                          df=stochastic_forcing_intensity,
-                          tau0=ocean_restoring_timescale,
-                          per0=ocean_oscillation_timescale,
-                          seed=seed,
-                          save_path=None
-                          )
+    spunge = spunge_ocean(
+        nt=time_steps,
+        dt=dt,
+        df=stochastic_forcing_intensity,
+        tau0=ocean_restoring_timescale,
+        seed=seed,
+        save_path=None,
+    )
+    oscillator = oscillatory_ocean(
+        nt=time_steps,
+        dt=dt,
+        df=stochastic_forcing_intensity,
+        tau0=ocean_restoring_timescale,
+        per0=ocean_oscillation_timescale,
+        seed=seed,
+        save_path=None,
+    )
     ds = xr.merge([spunge, oscillator])
 
     if save_path is not None:
@@ -255,37 +301,39 @@ def integrate_all(
     stochastic_forcing_intensity=1.15e-1,
     ocean_restoring_timescale=10 * 365.25,
     ocean_oscillation_timescale=24 * 365.25,
-    dNAO = 0.1, # (K days-1/2) stochastic amplitude of NAO
-    dEAP = 0.1, # (K days-1/2) stochastic amplitude of EAP
-    cNAOvsEAP = 0, # (K^2 days) Covariance of NAO and EAP
+    dNAO=0.1,  # (K days-1/2) stochastic amplitude of NAO
+    dEAP=0.1,  # (K days-1/2) stochastic amplitude of EAP
+    cNAOvsEAP=0,  # (K^2 days) Covariance of NAO and EAP
     save_path=None,
     seed=331381460666,
 ):
-    spunge = spunge_ocean(nt=time_steps,
-                          dt=dt,
-                          df=stochastic_forcing_intensity,
-                          tau0=ocean_restoring_timescale,
-                          seed=seed,
-                          save_path=None
-                          )
-    oscillator = oscillatory_ocean(nt=time_steps,
-                          dt=dt,
-                          df=stochastic_forcing_intensity,
-                          tau0=ocean_restoring_timescale,
-                          per0=ocean_oscillation_timescale,
-                          seed=seed,
-                          save_path=None
-                          )
-    rossby  = realistic_AMO_oscillatory_ocean(
+    spunge = spunge_ocean(
+        nt=time_steps,
+        dt=dt,
+        df=stochastic_forcing_intensity,
+        tau0=ocean_restoring_timescale,
+        seed=seed,
+        save_path=None,
+    )
+    oscillator = oscillatory_ocean(
+        nt=time_steps,
+        dt=dt,
+        df=stochastic_forcing_intensity,
+        tau0=ocean_restoring_timescale,
+        per0=ocean_oscillation_timescale,
+        seed=seed,
+        save_path=None,
+    )
+    rossby = AMO_oscillatory_ocean(
         nt=time_steps,
         dt=dt,
         tau0=ocean_restoring_timescale,
         per0=ocean_oscillation_timescale,
-        dNAO = dNAO, # (K days-1/2) stochastic amplitude of NAO
-        dEAP = dEAP, # (K days-1/2) stochastic amplitude of EAP
-        cNAOvsEAP = cNAOvsEAP,
+        dNAO=dNAO,  # (K days-1/2) stochastic amplitude of NAO
+        dEAP=dEAP,  # (K days-1/2) stochastic amplitude of EAP
+        cNAOvsEAP=cNAOvsEAP,
         seed=seed,
-        save_path=None
+        save_path=None,
     )
     ds = xr.merge([spunge, oscillator, rossby])
 
@@ -293,4 +341,6 @@ def integrate_all(
         ds.to_netcdf(save_path, mode="w")
     else:
         return ds
+
+
 # %%
