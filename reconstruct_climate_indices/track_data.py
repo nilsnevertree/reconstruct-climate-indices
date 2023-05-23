a92abc13868271dc30750b7225eae0573fbf1ef4
nilsnevertree <nils.niebaum@gmx.de>
2023-05-23 11:43:17 +0200

    allow for new directory creation

--- reconstruct_climate_indices/track_data.py
+++ reconstruct_climate_indices/track_data.py
@@ -1,4 +1,6 @@
 # %%
+import os
+
 from pathlib import Path
 from random import randint, random
 
@@ -13,18 +15,31 @@ from mlflow import (
 )
 
 
-def track_model(_func, mlflow_args={}, func_args={}, func_kwargs={}, subdata_path = "idealized_ocean"):
+def track_model(
+    _func, mlflow_args={}, func_args={}, func_kwargs={}, subdata_path="idealized_ocean"
+):
     REPO_PATH = Path(__file__).parent.parent.resolve()
     print(REPO_PATH)
-    DATA_PATH = REPO_PATH / "data"
+    DATA_PATH = REPO_PATH / "data" / subdata_path
     set_tracking_uri(REPO_PATH / "mlruns")
     with start_run(**mlflow_args) as run:
+        # log function name
         run_id = run.info.run_id
-        # perform model run
-        ds, setting = _func(*func_args, **func_kwargs)
-        log_params(setting)
+        log_param("FunctionName", _func.__name__)
+
         # log filepath and store file
         FILE_PATH = DATA_PATH / f"{run_id}.nc"
-        log_param("FileName", FILE_PATH.relative_to(DATA_PATH).as_posix())
+        log_param("FilePath", FILE_PATH.relative_to(DATA_PATH).as_posix())
+
+        # create directory if needed
+        if not os.path.exists(DATA_PATH):
+            os.makedirs(DATA_PATH)
+
+        # perform model run as save file
+        ds, setting = _func(*func_args, **func_kwargs)
+        log_params(setting)
         ds.to_netcdf(FILE_PATH)
+
+        log_artifacts(DATA_PATH.as_posix())
     end_run()
+    return ds
