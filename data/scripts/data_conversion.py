737a012dcacd13e2767a55465abc80ab98efa43b
nilsnevertree <nils.niebaum@gmx.de>
2023-06-20 11:02:11 +0200

    linting

--- data/scripts/data_conversion.py
+++ data/scripts/data_conversion.py
@@ -1,33 +1,41 @@
 """
-Converts given CiCMOD csv file into multiple xr.Datasets for each individual model. These will be stored under DESTINATION_DIR/climate_indices_``MODEL``.nc
+Converts given CiCMOD csv file into multiple xr.Datasets for each individual
+model.
+
+These will be stored under DESTINATION_DIR/climate_indices_``MODEL``.nc
 Function ``cicmod_csv_to_netcdf`` can also be imported.
 This is the main function of the file.
 """
 """
 options:
-  -h, --help            
+  -h, --help
         show this help message and exit
-  --data_url DATA_URL   
+  --data_url DATA_URL
         path or URL to the CiCMOD csv file. Default to ../earth_system_models/CiCMOD/climate_indices.csv
   --destination_dir DESTINATION_DIR
         path to directory to store all individual xr.Datasets for each model in the CiCMOD csv file. Default to ../earth_system_models/CiCMOD
 """
 
+import argparse
+
+from os import PathLike
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import xarray as xr
-from pathlib import Path
-from os import PathLike
-import argparse
 
- 
-def cicmod_csv_to_netcdf(data_url: PathLike, destination_dir: PathLike, file_name: str = "climate_indices") -> None:
+
+def cicmod_csv_to_netcdf(
+    data_url: PathLike, destination_dir: PathLike, file_name: str = "climate_indices"
+) -> None:
     """
-    Convert climate indices data from a CSV file to NetCDF format and save to the destination directory.
-    It is made for converting the CiCMOD dataset csv results into netCDF.
-    For each model present in the csv file a seperate file will be created:
-    Example of csv file with two models "FOCI" and "CESM":
+    Convert climate indices data from a CSV file to NetCDF format and save to
+    the destination directory. It is made for converting the CiCMOD dataset csv
+    results into netCDF. For each model present in the csv file a seperate file
+    will be created: Example of csv file with two models "FOCI" and "CESM":
     Output will be two files with Paths:
+
         - ``destination_dir`` / ``file_name``_CESM.nc
         - ``destination_dir`` / ``file_name``_FOCI.nc
 
@@ -50,11 +58,11 @@ def cicmod_csv_to_netcdf(data_url: PathLike, destination_dir: PathLike, file_nam
             for year, month in zip(years, months)
         ]
         ds = ds.assign_coords(time=(["year"], time))
-        
+
         store the data
 
     Examples:
-        # Example 1: Convert climate indices data from CSV to NetCDF assume two models in the ``climate_indices.csv`` file : "CSEM" and "FOCI"  
+        # Example 1: Convert climate indices data from CSV to NetCDF assume two models in the ``climate_indices.csv`` file : "CSEM" and "FOCI"
         >>> data_url = "path/to/climate_indices.csv"
         >>> destination_dir = "path/to/destination"
         >>> file_name = "climate_data"
@@ -72,30 +80,34 @@ def cicmod_csv_to_netcdf(data_url: PathLike, destination_dir: PathLike, file_nam
 
     climind = pd.read_csv(data_url)
     # Set index:
-    climind = climind.set_index(["model", "year", "month", "index"]).unstack(level=-1)["value"]
+    climind = climind.set_index(["model", "year", "month", "index"]).unstack(level=-1)[
+        "value"
+    ]
 
-    model_index = climind.index.get_level_values('model')
+    model_index = climind.index.get_level_values("model")
     available_models = model_index.unique()
     for model in available_models:
         # select the dataframe for each model
         df = climind.loc[model]
         # extract years and months
-        years = np.array(df.index.get_level_values('year'))
-        months = np.array(df.index.get_level_values('month'))
+        years = np.array(df.index.get_level_values("year"))
+        months = np.array(df.index.get_level_values("month"))
         year_index = years + (months - 1) / 12
 
         # create time index.
         # NOTE: Other dimension names are not yet supported by the kalman_reconstruction.pipeline module
         df = df.set_index(year_index)
-        df.index.names = ['time']
+        df.index.names = ["time"]
 
         # create the xr.Dataset for each model
         ds = xr.Dataset(df)
-        ds.assign_attrs(dict(
-            model=model,
-            time_unit="Units in years.\nThe decimals correspond to the fraction of the month compared to 12.\n"
-                      "Example: year 5 month 6 -> 'time' = 5 + 6/12 = 5.5"
-        ))
+        ds.assign_attrs(
+            dict(
+                model=model,
+                time_unit="Units in years.\nThe decimals correspond to the fraction of the month compared to 12.\n"
+                "Example: year 5 month 6 -> 'time' = 5 + 6/12 = 5.5",
+            )
+        )
 
         # store the data
         ds.to_netcdf(destination_data(model))
--- results/scripts/plot_CiCMOD.py
+++ results/scripts/plot_CiCMOD.py
@@ -4,7 +4,7 @@ The settings will be stored as "settings.yaml" in the ``results_path``.
 Also multiple plots will be created and stored in the same directory.
 
 
-An example would be : 
+An example would be :
 python plot_CiCMOD.py --file_name climate_indices_FOCI.nc --experiment_name FOCI_default
 
 NOTE:
@@ -23,18 +23,18 @@ parser.add_argument(
     help="The filename should be is usually ``climate_indices_{model}.nc``.",
 )
 parser.add_argument(
-    "--dir_path", 
-    default= Path("data") / Path("earth_system_models") / Path("CiCMOD"),
+    "--dir_path",
+    default=Path("data") / Path("earth_system_models") / Path("CiCMOD"),
     help="The source directory path where the netCDF file is stored.\n Default to ``.data/earth_system_models/CiCMOD``",
 )
 parser.add_argument(
-    "--results_path", 
-    default= Path("results") / Path("CiCMOD"),
+    "--results_path",
+    default=Path("results") / Path("CiCMOD"),
     help="The name to store the figures. \n Default to ``results/CiCMOD``",
 )
 parser.add_argument(
-    "--experiment_name", 
-    default= Path("default"),
+    "--experiment_name",
+    default=Path("default"),
     help="The name of directory below ``results_path`` to store the figures in. \n Default to ``default``",
 )
 
@@ -42,16 +42,24 @@ args = parser.parse_args()
 print("Start imports.")
 
 
-import xarray as xr
-import numpy as np
-from kalman_reconstruction import pipeline
-from kalman_reconstruction.custom_plot import ncols_nrows_from_N, set_custom_rcParams, plot_state_with_probability, adjust_lightness
-from kalman_reconstruction.statistics import normalize, compute_fft_spectrum, crosscorr
-import matplotlib.pyplot as plt
-import yaml
 # from scipy import fftpack
 from typing import Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import yaml
+
+from kalman_reconstruction import pipeline
+from kalman_reconstruction.custom_plot import (
+    adjust_lightness,
+    ncols_nrows_from_N,
+    plot_state_with_probability,
+    set_custom_rcParams,
+)
+from kalman_reconstruction.statistics import compute_fft_spectrum, crosscorr, normalize
 from tqdm import tqdm
+
 
 set_custom_rcParams()
 plt.rcParams["axes.grid"] = True
@@ -74,6 +82,8 @@ results_path = REPO_PATH / Path(results_path) / Path(experiment_name)
 results_path.mkdir(parents=True, exist_ok=True)
 
 SAVE_FIGURES = True
+
+
 def save_fig(fig, relative_path, **kwargs):
     store_path = results_path / relative_path
     store_path.parent.mkdir(parents=True, exist_ok=True)
@@ -85,32 +95,34 @@ def save_fig(fig, relative_path, **kwargs):
 
 # %%
 # Set the Kalman and plot settings
-time_slice = slice(0,201)
-rolling_window = 10*12
+time_slice = slice(0, 201)
+rolling_window = 10 * 12
 # Set Kalman-SEM settings
 rng_seed = 834567
 random_variance = 1
 nb_iter_SEM = 25
-observation_variables=["AMO", "PDO_PC", "NAO_PC"]
+observation_variables = ["AMO", "PDO_PC", "NAO_PC"]
 state_variables = ["AMO", "PDO_PC", "NAO_PC", "latent"]
 smoothed_variables = ["AMO"]
 
 settings = dict(
     rolling_window=rolling_window,
     rng_seed=rng_seed,
-    random_variance= random_variance,
+    random_variance=random_variance,
     observation_variables=observation_variables,
     state_variables=state_variables,
-    smoothed_variables=smoothed_variables, 
-    nb_iter_SEM= nb_iter_SEM,
-    time_slice= dict(
-        start = time_slice.start,
-        stop = time_slice.stop,
+    smoothed_variables=smoothed_variables,
+    nb_iter_SEM=nb_iter_SEM,
+    time_slice=dict(
+        start=time_slice.start,
+        stop=time_slice.stop,
     ),
-    data_path = str(data_path)
+    data_path=str(data_path),
 )
 with open(results_path / "settings.yaml", "w") as stream:
-    stream.write("#Settings used in the application of the Kalman_SEM on the CiCOD dataset.\n")
+    stream.write(
+        "#Settings used in the application of the Kalman_SEM on the CiCOD dataset.\n"
+    )
     yaml.dump(data=settings, stream=stream, default_flow_style=False)
 
 
@@ -118,11 +130,11 @@ with open(results_path / "settings.yaml", "w") as stream:
 data_original = xr.load_dataset(data_path)
 
 # %%
-data= data_original.sel(time = time_slice).copy()
-try : 
+data = data_original.sel(time=time_slice).copy()
+try:
     for var in smoothed_variables:
-        data[var] = data[var].rolling(time = rolling_window).mean()
-    data = data.isel(time = slice(rolling_window,-rolling_window))
+        data[var] = data[var].rolling(time=rolling_window).mean()
+    data = data.isel(time=slice(rolling_window, -rolling_window))
 except:
     pass
 
@@ -135,42 +147,49 @@ for random_var in random_vars:
         var_name=random_var,
         random_generator=rng,
         variance=random_variance,
-        dim = "time",
+        dim="time",
     )
 
 
 # %%
 kalman_results = pipeline.xarray_Kalman_SEM(
-    ds= data,
+    ds=data,
     observation_variables=observation_variables,
-    state_variables = state_variables,
-    nb_iter_SEM = nb_iter_SEM
+    state_variables=state_variables,
+    nb_iter_SEM=nb_iter_SEM,
 )
 
 # %%
 kalman_states = pipeline.from_standard_dataset(kalman_results)
 
 # %% Plot loglikelihood
-fig, ax = plt.subplots(1, 1, figsize = (8,4))
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
 ax.plot(kalman_results.kalman_itteration, kalman_results.log_likelihod)
 ax.set_xlabel("kalman itteration")
 ax.set_ylabel("log likelihood")
-fig.suptitle("CiCMOD | Loglikelihood Kalman SEM");
-save_fig(fig, "CiCMOD_loglikelihood.png", dpi = 400)
+fig.suptitle("CiCMOD | Loglikelihood Kalman SEM")
+save_fig(fig, "CiCMOD_loglikelihood.png", dpi=400)
 
 
 # %% Plot scatter truth against reconstruction
-fig, axs = plt.subplots(nrows = len(state_variables), ncols =  1, layout = "constrained", figsize = (12,7), sharex=True)
+fig, axs = plt.subplots(
+    nrows=len(state_variables),
+    ncols=1,
+    layout="constrained",
+    figsize=(12, 7),
+    sharex=True,
+)
 axs = axs.flatten()
 for idx, var in enumerate(state_variables):
     handle1, handle2 = plot_state_with_probability(
-        ax = axs[idx],
+        ax=axs[idx],
         x_value=kalman_results.time,
-        state=kalman_results.states.sel(state_name = var),
-        prob=kalman_results.covariance.sel(state_name = var, state_name_copy = var),
-        line_kwargs=dict(label = f"{var}"),
-        output = True)
-    
+        state=kalman_results.states.sel(state_name=var),
+        prob=kalman_results.covariance.sel(state_name=var, state_name_copy=var),
+        line_kwargs=dict(label=f"{var}"),
+        output=True,
+    )
+
     # if "latent" not in var:
     # color = adjust_lightness(handle1[0].get_color(), )
     # axs[idx].plot(data.time, data[var], label = f"{var} truth", alpha = 0.7, linestyle = ":")
@@ -179,7 +198,7 @@ for idx, var in enumerate(state_variables):
     axs[idx].legend()
 axs[idx].set_xlabel("time in years")
 fig.suptitle("CiCMOD | Reconstruction against truth")
-save_fig(fig, "CiCMOD_recons_truth.png", dpi = 400)
+save_fig(fig, "CiCMOD_recons_truth.png", dpi=400)
 
 # %% Plot scatter truth against reconstruction
 # n_cols = len(state_variables)
@@ -202,35 +221,35 @@ save_fig(fig, "CiCMOD_recons_truth.png", dpi = 400)
 data_vars = data.data_vars
 row_col = ncols_nrows_from_N(len(data_vars))
 
-fig, axs = plt.subplots(layout = "constrained", figsize = (20,20),**row_col)
+fig, axs = plt.subplots(layout="constrained", figsize=(20, 20), **row_col)
 axs = axs.flatten()
 
 for idx, var in enumerate(data_vars):
     axs[idx].scatter(
         normalize(kalman_states.latent),
         normalize(data[var]),
-        marker = ".",
-        alpha = 0.75,
+        marker=".",
+        alpha=0.75,
     )
     axs[idx].set_xlabel(var)
     axs[idx].set_ylabel("variable")
     axs[idx].set_title(var)
 
 fig.suptitle("CiCMOD | Latent variable against climate indeces")
-save_fig(fig, "CiCMOD_latent_indices_scatter.png", dpi = 400)
+save_fig(fig, "CiCMOD_latent_indices_scatter.png", dpi=400)
 
 # %% [markdown]
-### Compute lagged cross correlation and covariance  
+### Compute lagged cross correlation and covariance
 
 # %% Calculate lagged cross correlation for all variables in the CiCMOD dataset against ``latent``
-lag_years = np.arange(-30,30,1)
+lag_years = np.arange(-30, 30, 1)
 data_vars = data.data_vars
 data_ccor_list = []
 kalman_ccor_list = []
 for idx, var in tqdm(enumerate(data_vars)):
     for lag in lag_years:
         # because data is stored in monthly form, we need to multiply the shift by 12 to have teh lag in years
-        lag_months = lag*12
+        lag_months = lag * 12
         # # No need to calculate covarinace
         # # calculate the covariance
         # ccov = xr.cov(data[var], kalman_states.latent.shift(time=lag*12), dim = "time").values
@@ -246,56 +265,61 @@ for idx, var in tqdm(enumerate(data_vars)):
 
         # calculate the lagged cross correlation
         data_ccor = crosscorr(
-            ds1 = data[var], 
-            ds2 = kalman_states.latent, 
-            lag = lag_months, 
-            dim = "time"
-            )
+            ds1=data[var], ds2=kalman_states.latent, lag=lag_months, dim="time"
+        )
         # create the DataArray
         data_ccor = xr.DataArray(
-            data = data_ccor.values[np.newaxis],
+            data=data_ccor.values[np.newaxis],
             dims=["lag_years"],
-            coords = dict(
-                lag_years = (["lag_years"], [lag]),
-            )
+            coords=dict(
+                lag_years=(["lag_years"], [lag]),
+            ),
         )
-        data_ccor = data_ccor.rename(var)        
+        data_ccor = data_ccor.rename(var)
         data_ccor_list.append(data_ccor)
-        # Also for all varibales try to make cross correaltino with the results from the kalman SEM 
-        try : 
+        # Also for all varibales try to make cross correaltino with the results from the kalman SEM
+        try:
             kalman_ccor = crosscorr(
-                ds1 = kalman_states[var], 
-                ds2 = kalman_states.latent, 
-                lag = lag_months, 
-                dim = "time"
-                )
-            # Create the DataArray 
+                ds1=kalman_states[var],
+                ds2=kalman_states.latent,
+                lag=lag_months,
+                dim="time",
+            )
+            # Create the DataArray
             kalman_ccor = xr.DataArray(
-                data = kalman_ccor.values[np.newaxis],
+                data=kalman_ccor.values[np.newaxis],
                 dims=["lag_years"],
-                coords = dict(
-                    lag_years = (["lag_years"], [lag]),
-                )
+                coords=dict(
+                    lag_years=(["lag_years"], [lag]),
+                ),
             )
             kalman_ccor = kalman_ccor.rename(var)
             kalman_ccor_list.append(kalman_ccor)
-        except :
+        except:
             pass
 
 # Merge all DataArrays to Datasets
-data_ccor = xr.merge(data_ccor_list)            # lagged cross correlation with all CICMOD vars
-kalman_ccor = xr.merge(kalman_ccor_list)        # lagged cross correlation with all vars avaibales in the kalman results.
+data_ccor = xr.merge(data_ccor_list)  # lagged cross correlation with all CICMOD vars
+kalman_ccor = xr.merge(
+    kalman_ccor_list
+)  # lagged cross correlation with all vars avaibales in the kalman results.
 
 # %% STATE VARIABLES | Plot Lagged correlation of latent variable against state varibales
 data_vars = state_variables
 row_col = ncols_nrows_from_N(len(data_vars))
 
-fig, axs = plt.subplots(layout = "constrained", figsize = (12,4), sharex=True, sharey=True, ncols = len(data_vars))
+fig, axs = plt.subplots(
+    layout="constrained",
+    figsize=(12, 4),
+    sharex=True,
+    sharey=True,
+    ncols=len(data_vars),
+)
 axs = axs.flatten()
 
 for idx, var in enumerate(data_vars):
-    axs[idx].step(data_ccor.lag_years, np.abs(data_ccor[var]), label = "turth")
-    axs[idx].step(kalman_ccor.lag_years, np.abs(kalman_ccor[var]), label = "recons")
+    axs[idx].step(data_ccor.lag_years, np.abs(data_ccor[var]), label="turth")
+    axs[idx].step(kalman_ccor.lag_years, np.abs(kalman_ccor[var]), label="recons")
     axs[idx].set_xlabel("lag in years")
     axs[idx].set_ylabel("correlation")
     axs[idx].set_title(var)
@@ -303,22 +327,26 @@ for idx, var in enumerate(data_vars):
 
 axs[idx].set_ylim((-1, 1))
 
-fig.suptitle("CiCMOD | Absolute value of lagged correlation of latent variable against state varibales");
-save_fig(fig, "CiCMOD_latent_states_lagged_corr.png", dpi = 400)
+fig.suptitle(
+    "CiCMOD | Absolute value of lagged correlation of latent variable against state varibales"
+)
+save_fig(fig, "CiCMOD_latent_states_lagged_corr.png", dpi=400)
 
 
-#%% ALL INDICES | lagged correlation of latent variable against state varibales
+# %% ALL INDICES | lagged correlation of latent variable against state varibales
 data_vars = data.data_vars
 row_col = ncols_nrows_from_N(len(data_vars))
 
-fig, axs = plt.subplots(layout = "constrained", figsize = (20,20), sharex=True, sharey=True, **row_col)
+fig, axs = plt.subplots(
+    layout="constrained", figsize=(20, 20), sharex=True, sharey=True, **row_col
+)
 axs = axs.flatten()
 
 for idx, var in enumerate(data_vars):
-    axs[idx].step(data_ccor.lag_years, data_ccor[var], label = "turth")
-    try : 
-        axs[idx].step(kalman_ccor.lag_years, kalman_ccor[var], label = "recons")
-    except : 
+    axs[idx].step(data_ccor.lag_years, data_ccor[var], label="turth")
+    try:
+        axs[idx].step(kalman_ccor.lag_years, kalman_ccor[var], label="recons")
+    except:
         pass
     axs[idx].set_xlabel("lag in years")
     axs[idx].set_ylabel("correlation")
@@ -328,21 +356,23 @@ for idx, var in enumerate(data_vars):
 extend = np.max(np.abs(axs[idx].get_ylim()))
 axs[idx].set_ylim((-extend, extend))
 
-fig.suptitle("CiCMOD | Lagged correlation of latent against climate indices.");
-save_fig(fig, "CiCMOD_latent_indices_lagged_corr.png", dpi = 400)
+fig.suptitle("CiCMOD | Lagged correlation of latent against climate indices.")
+save_fig(fig, "CiCMOD_latent_indices_lagged_corr.png", dpi=400)
 
 # %% ALL INDICES | Absolute value of lagged correlation of latent variable against state varibales
 data_vars = data.data_vars
 row_col = ncols_nrows_from_N(len(data_vars))
 
-fig, axs = plt.subplots(layout = "constrained", figsize = (20,20), sharex=True, sharey=True, **row_col)
+fig, axs = plt.subplots(
+    layout="constrained", figsize=(20, 20), sharex=True, sharey=True, **row_col
+)
 axs = axs.flatten()
 
 for idx, var in enumerate(data_vars):
-    axs[idx].step(data_ccor.lag_years, np.abs(data_ccor[var]), label = "turth")
-    try : 
-        axs[idx].step(kalman_ccor.lag_years, np.abs(kalman_ccor[var]), label = "recons")
-    except : 
+    axs[idx].step(data_ccor.lag_years, np.abs(data_ccor[var]), label="turth")
+    try:
+        axs[idx].step(kalman_ccor.lag_years, np.abs(kalman_ccor[var]), label="recons")
+    except:
         pass
     axs[idx].set_xlabel("lag in years")
     axs[idx].set_ylabel("correlation")
@@ -351,8 +381,10 @@ for idx, var in enumerate(data_vars):
 
 axs[idx].set_ylim((0, 1))
 
-fig.suptitle("CiCMOD | Absolute value lagged correlation of latent against climate indices.");
-save_fig(fig, "CiCMOD_latent_indices_lagged_corr_abs.png", dpi = 400)
+fig.suptitle(
+    "CiCMOD | Absolute value lagged correlation of latent against climate indices."
+)
+save_fig(fig, "CiCMOD_latent_indices_lagged_corr_abs.png", dpi=400)
 
 
 # %% [markdown]
@@ -360,22 +392,32 @@ save_fig(fig, "CiCMOD_latent_indices_lagged_corr_abs.png", dpi = 400)
 
 # %% Frequency analysis of all state variables.
 data_vars = state_variables
-fig, axs = plt.subplots(layout = "constrained", figsize = (12,4), sharex=True, sharey=True, ncols = len(data_vars))
+fig, axs = plt.subplots(
+    layout="constrained",
+    figsize=(12, 4),
+    sharex=True,
+    sharey=True,
+    ncols=len(data_vars),
+)
 axs = axs.flatten()
 
 for idx, var in enumerate(data_vars):
     ax = axs[idx]
-    xf, yf, yf_plot, f_min , f_max = compute_fft_spectrum(time = data.time, signal = data[var].values)
-    ax.loglog(xf, yf_plot, label = "truth", alpha = 0.7)
-    (xf, yf, yf_plot, f_min , f_max) = compute_fft_spectrum(time = kalman_states.time, signal = kalman_states[var].values)
-    ax.loglog(xf, yf_plot, label = "reconst.", alpha = 0.7)
+    xf, yf, yf_plot, f_min, f_max = compute_fft_spectrum(
+        time=data.time, signal=data[var].values
+    )
+    ax.loglog(xf, yf_plot, label="truth", alpha=0.7)
+    (xf, yf, yf_plot, f_min, f_max) = compute_fft_spectrum(
+        time=kalman_states.time, signal=kalman_states[var].values
+    )
+    ax.loglog(xf, yf_plot, label="reconst.", alpha=0.7)
 
-    f_min = 1/200 #  years^{-1}
+    f_min = 1 / 200  #  years^{-1}
     ax.set_xlim((f_min, f_max))
     xticks = ax.get_xticks().copy()  # 1/years
-    new_ticks = np.round(1/xticks, decimals = 2) # years
-    ax.set_xticks(ticks=xticks, labels= new_ticks)
-    ax.tick_params(axis='x', rotation=45)
+    new_ticks = np.round(1 / xticks, decimals=2)  # years
+    ax.set_xticks(ticks=xticks, labels=new_ticks)
+    ax.tick_params(axis="x", rotation=45)
     ax.set_xlim((f_min, f_max))
 
     ax.set_xlabel(r"Period in years")
@@ -383,8 +425,6 @@ for idx, var in enumerate(data_vars):
     ax.set_title(var)
     ax.legend()
 
-ax.set_ylim(bottom= 10**(-5.5), top = 10**(0.5))
-fig.suptitle("CiCMOD | Frequency spectrum state varibales");
-save_fig(fig, "CiCMOD_fft_states.png", dpi = 400)
-
-
+ax.set_ylim(bottom=10 ** (-5.5), top=10 ** (0.5))
+fig.suptitle("CiCMOD | Frequency spectrum state varibales")
+save_fig(fig, "CiCMOD_fft_states.png", dpi=400)
